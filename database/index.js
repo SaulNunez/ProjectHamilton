89174872abb68b1a14ebdda147d86ed19b0b3d1c
Saul Nuñez <saul.nunez99@gmail.com>
2020-08-05 01:08:23 -0600

    Schema para la base de datos

--- database/index.js
+++ database/index.js
@@ -1,11 +1,52 @@
-const knex = require('knex')({
+const db = require('knex')({
     client: 'mysql',
     connection: {
-      host : 'PersistDb',
-      user : 'Temp',
-      password : 'password',
-      database : 'ProjectHamilton'
+        host: 'PersistDb',
+        user: 'Temp',
+        password: 'password',
+        database: 'ProjectHamilton'
     }
-  });
+});
 
-export default knex;
+db.schema
+    .createTableIfNotExists('lobbies', table => {
+        table.string("code").unique();
+        table.dateTime("creation_time");
+    })
+    .createTableIfNotExists('items', table => {
+        table.uuid("item_token").unique();
+        table.integer("sanity");
+        table.integer("physical");
+        table.integer("intelligence");
+        table.integer("bravery");
+        table.string("name");
+    })
+    .createTableIfNotExists('players', table => {
+        table.integer("x");
+        table.integer("y");
+        table.integer("floor");
+        table.integer("sanity");
+        table.integer("physical");
+        table.integer("intelligence");
+        table.integer("bravery");
+        table.integer("turn_throw");
+        table.integer("current_movement_throw");
+        table.uuid("server_token");
+        table.string("name");
+        table.string("lobby")
+        .references("lobbies.code");
+    })
+    .createTableIfNotExists('rooms', table => {
+        table.integer("x");
+        table.integer("y");
+        table.integer("floor");
+        table.string("lobby")
+            .references("lobbies.code");
+        table.enu("gameplay_effect", ["none", "puzzle", "item"]);
+        table.integer("stat_effects_physical");
+        table.integer("stat_effects_sanity");
+        table.integer("stat_effects_intelligence");
+        table.integer("stat_effects_bravery");
+    });
+
+export default db;

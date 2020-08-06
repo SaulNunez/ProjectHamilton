d8df38bddde53c8abc6923b6948ca8a9180622c1
Saul Nuñez <saul.nunez99@gmail.com>
2020-08-05 18:44:26 -0600

    Retornar codigo en la api

--- index.js
+++ index.js
@@ -5,14 +5,21 @@ const port = 3000;
 
 import db from './database/index.js';
 
-app.post("create_lobby", (request, response) => {
-    let lobbyCode = Math.random().toString(36).substring(7);
+app.post("/create_lobby", (request, response) => {
+    try{
+        let lobbyCode = Math.random().toString(36).substring(7);
     
-    db("lobbies").insert({
-        code: lobbyCode,
-    });
-    
-    //Crear cuartos del lobby
+        db("lobbies").insert({
+            code: lobbyCode,
+        });
+
+        //TODO: Crear cuartos del lobby
+
+        response.send({code: lobbyCode});
+    } catch(e){
+        console.error(e);
+        response.status(400).send({message: "Algo salio mal, porfavor intenta de nuevo."});
+    }
 });
 
 app.use(express.static('public'));

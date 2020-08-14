9b7ea54d5d3354b581100a27a9892e794c785205
Saul Nuñez <saul.nunez99@gmail.com>
2020-08-13 21:25:48 -0600

    Usar una extension intermedia para usar websockets con Express.

--- index.js
+++ index.js
@@ -1,6 +1,7 @@
 const express = require('express');
-const WebSocket = require('ws');
 const app = express();
+
+var expressWs = require('express-ws')(app);
 
 import db from './database/index.js';
 
@@ -21,97 +22,94 @@ app.post("/create_lobby", (request, response) => {
     }
 });
 
-app.use(express.static('public'));
-
-const wss = new WebSocket.Server({ server: app });
- 
-wss.on('connection', (ws) => {
-  ws.on('message', async (message) => {
-    const messageContents = JSON.parse(message);
-
-    switch(messageContents.type){
-        case 'enter_lobby':
-            const [lobbyCode] = messageContents.payload;
-            
-            const results = await db('lobbies').select('code').where({code: lobbyCode});
-            try {
-                if(results.length > 0){
-                    const playerInfoQuery = await db('players').select('character', 'name').where({lobby: lobbyCode});
-
-                    const tokenInfo = await db.insert({lobby: lobbyCode}, 'token').into('players');
+app.ws('/gameapi', function(ws, req) {
+    ws.on('message', async (message) => {
+        const messageContents = JSON.parse(message);
     
+        switch(messageContents.type){
+            case 'enter_lobby':
+                const [lobbyCode] = messageContents.payload;
+                
+                const results = await db('lobbies').select('code').where({code: lobbyCode});
+                try {
+                    if(results.length > 0){
+                        const playerInfoQuery = await db('players').select('character', 'name').where({lobby: lobbyCode});
+    
+                        const tokenInfo = await db.insert({lobby: lobbyCode}, 'token').into('players');
+        
+                        ws.send({
+                            type: 'lobby_joined',
+                            payload: {
+                                currentPlayers: playerInfoQuery.length,
+                                charactersUsed: playerInfoQuery.map(playerInfo => playerInfo.character).filter(player => player),
+                                playersInLobbyInfo: playerInfoQuery,
+                                token: tokenInfo.token
+                            }
+                        });
+                    } else {
+                        ws.send({
+                            type: 'lobby_joined',
+                            error: 'There\'s no lobby found with that code'
+                        });
+                    }
+                } catch(error){
                     ws.send({
                         type: 'lobby_joined',
+                        error: error.message
+                    });
+                }
+    
+                break;
+            case "get_available_characters":
+                try{
+                    const [lobbyCode] = messageContents.payload;
+                    const playerInfoQuery = await db('players').select('character', 'name').where({lobby: lobbyCode});
+    
+                    ws.send({
+                        type: 'available_characters_update',
                         payload: {
                             currentPlayers: playerInfoQuery.length,
                             charactersUsed: playerInfoQuery.map(playerInfo => playerInfo.character).filter(player => player),
                             playersInLobbyInfo: playerInfoQuery,
-                            token: tokenInfo.token
                         }
                     });
-                } else {
+                } catch(error){
                     ws.send({
-                        type: 'lobby_joined',
-                        error: 'There\'s no lobby found with that code'
+                        type: 'available_characters_update',
+                        error: error.message
                     });
                 }
-            } catch(error){
+                break;
+            case "select_character":
+                const [token, displayName, character] = messageContents.payload;
+    
+                db('players').where('token', '=', token).update({name: displayName, character: character});
+    
                 ws.send({
-                    type: 'lobby_joined',
-                    error: error.message
+                    type: 'player_selection_sucess'
                 });
-            }
-
-            break;
-        case "get_available_characters":
-            try{
-                const [lobbyCode] = messageContents.payload;
-                const playerInfoQuery = await db('players').select('character', 'name').where({lobby: lobbyCode});
-
-                ws.send({
-                    type: 'available_characters_update',
+    
+                const playerInfoQuery = await db('players').select('character', 'name', 'x', 'y', 'floor').where({lobby: lobbyCode});
+                const data ={
+                    type: 'player_selected_character',
                     payload: {
                         currentPlayers: playerInfoQuery.length,
-                        charactersUsed: playerInfoQuery.map(playerInfo => playerInfo.character).filter(player => player),
-                        playersInLobbyInfo: playerInfoQuery,
+                        playersInLobbyInfo: playerInfoQuery.map(({name, character})  => ({name, character})),
+                        playersPosition: playerInfoQuery.map(({character, x, y, floor}) => ({character, x, y, floor}))
                     }
-                });
-            } catch(error){
-                ws.send({
-                    type: 'available_characters_update',
-                    error: error.message
-                });
-            }
-            break;
-        case "select_character":
-            const [token, displayName, character] = messageContents.payload;
-
-            db('players').where('token', '=', token).update({name: displayName, character: character});
-
-            ws.send({
-                type: 'player_selection_sucess'
-            });
-
-            const playerInfoQuery = await db('players').select('character', 'name', 'x', 'y', 'floor').where({lobby: lobbyCode});
-            const data ={
-                type: 'player_selected_character',
-                payload: {
-                    currentPlayers: playerInfoQuery.length,
-                    playersInLobbyInfo: playerInfoQuery.map(({name, character})  => ({name, character})),
-                    playersPosition: playerInfoQuery.map(({character, x, y, floor}) => ({character, x, y, floor}))
-                }
-            };
-
-            wss.clients.forEach(function each(client) {
-                if (client !== ws && client.readyState === WebSocket.OPEN) {
-                  client.send(data);
-                }
-              });
-            break;
-    }
+                };
+    
+                wss.clients.forEach(function each(client) {
+                    if (client !== ws && client.readyState === WebSocket.OPEN) {
+                      client.send(data);
+                    }
+                  });
+                break;
+        }
   });
-
 });
+
+app.use(express.static('public'));
 
 const port = process.env.PORT || 3000;
 

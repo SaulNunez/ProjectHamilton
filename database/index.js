a51001ed8fe43210b7eabde3d99566d34fdfce21
Saul Nuñez <saul.nunez99@gmail.com>
2020-08-19 10:48:31 -0600

    Agregar algunos constraints, así como estandarizar algunos nombres

--- database/index.js
+++ database/index.js
@@ -28,43 +28,45 @@ const db = require('knex')({
 db.schema
     .createTable('lobbies', table => {
         table.string("code").unique();
-        table.dateTime("creation_time");
+        table.timestamp('creation_time').notNullable().defaultTo(knex.fn.now());
     })
     .createTable('items', table => {
-        table.uuid("item_token").unique();
-        table.integer("sanity");
-        table.integer("physical");
-        table.integer("intelligence");
-        table.integer("bravery");
+        table.uuid("id").unique();
+        table.uuid("player_id").references("players.id");
+        table.integer("sanity_effect").notNullable().defaultTo(0);
+        table.integer("physical_effect").notNullable().defaultTo(0);
+        table.integer("intelligence_effect").notNullable().defaultTo(0);
+        table.integer("bravery_effect").notNullable().defaultTo(0);
         table.string("name");
     })
     .createTable('players', table => {
-        table.integer("x");
-        table.integer("y");
-        table.integer("floor");
-        table.integer("sanity");
-        table.integer("physical");
-        table.integer("intelligence");
-        table.integer("bravery");
+        table.uuid("id");
+        table.integer("x").notNullable().defaultTo(0);
+        table.integer("y").notNullable().defaultTo(0);
+        table.integer("floor").notNullable().defaultTo(0);
+        table.integer("sanity").notNullable();
+        table.integer("physical").notNullable();
+        table.integer("intelligence").notNullable();
+        table.integer("bravery").notNullable();
         table.integer("turn_throw");
         table.integer("current_movement_throw");
-        table.uuid("token");
         table.string("name");
         table.string("character");
-        table.string("lobby")
+        table.string("lobby_id")
         .references("lobbies.code");
     })
     .createTable('rooms', table => {
-        table.integer("x");
-        table.integer("y");
-        table.integer("floor");
-        table.string("lobby")
+        table.uuid("id");
+        table.integer("x").notNullable();
+        table.integer("y").notNullable();
+        table.integer("floor").notNullable();
+        table.string("lobby_id")
             .references("lobbies.code");
-        table.enu("gameplay_effect", ["none", "puzzle", "item"]);
-        table.integer("stat_effects_physical");
-        table.integer("stat_effects_sanity");
-        table.integer("stat_effects_intelligence");
-        table.integer("stat_effects_bravery");
+        table.enu("gameplay_effect", ["none", "puzzle", "item", "event"]);
+        table.integer("stat_effects_physical").notNullable().defaultTo(0);
+        table.integer("stat_effects_sanity").notNullable().defaultTo(0);
+        table.integer("stat_effects_intelligence").notNullable().defaultTo(0);
+        table.integer("stat_effects_bravery").notNullable().defaultTo(0);
     });
 
 export default db;

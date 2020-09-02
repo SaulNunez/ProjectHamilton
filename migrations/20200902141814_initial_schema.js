7faa833ccdd6a51b2e3498ebe4b10be32f1f4def
Saul Nuñez <saul.nunez99@gmail.com>
2020-09-02 16:26:00 -0600

    Corregir orden en la que se crean tablas en la migracion

--- migrations/20200902141814_initial_schema.js
+++ migrations/20200902141814_initial_schema.js
@@ -5,30 +5,30 @@ exports.up = function(knex) {
       table.string("code").unique();
       table.timestamp('creation_time').notNullable().defaultTo(knex.fn.now());
   })
+  .createTable('players', table => {
+    table.uuid("id");
+    table.integer("x").notNullable().defaultTo(0);
+    table.integer("y").notNullable().defaultTo(0);
+    table.integer("floor").notNullable().defaultTo(0);
+    table.integer("sanity").notNullable();
+    table.integer("physical").notNullable();
+    table.integer("intelligence").notNullable();
+    table.integer("bravery").notNullable();
+    table.integer("turn_throw");
+    table.integer("current_movement_throw");
+    table.string("display_name");
+    table.string("character_prototype_id");
+    table.string("character_name");
+    table.string("lobby_id")
+    .references("lobbies.code");
+
+    table.unique(['lobby_id', 'character_prototype_id']);
+})
   .createTable('items', table => {
       table.uuid("id").unique();
       table.uuid("player_id").references("players.id");
       table.integer("prototype");
       table.string("name");
-  })
-  .createTable('players', table => {
-      table.uuid("id");
-      table.integer("x").notNullable().defaultTo(0);
-      table.integer("y").notNullable().defaultTo(0);
-      table.integer("floor").notNullable().defaultTo(0);
-      table.integer("sanity").notNullable();
-      table.integer("physical").notNullable();
-      table.integer("intelligence").notNullable();
-      table.integer("bravery").notNullable();
-      table.integer("turn_throw");
-      table.integer("current_movement_throw");
-      table.string("display_name");
-      table.string("character_prototype_id");
-      table.string("character_name");
-      table.string("lobby_id")
-      .references("lobbies.code");
-
-      table.unique(['lobby_id', 'character_prototype_id']);
   })
   .createTable('rooms', table => {
       table.uuid("id");

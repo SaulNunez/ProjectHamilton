d6112b4e697bf0f1dddaa2b534a0ce26ff4f3173
Saul Nuñez <saul.nunez99@gmail.com>
2020-09-08 01:01:25 -0600

    Actualizar cliente

---
+++ gameassets/puzzles/index.js
@@ -0,0 +1,18 @@
+export const NO_ANALYZE = 'no_analyze';
+export const CONDITIONAL = 'conditionals';
+export const CYCLES = 'cycles';
+export const FUNCTIONS = 'functions';
+
+export default [
+    {
+        id: '',
+        type: '',
+        defaultWorkspace: null,
+        instructions: "",
+        expectedOutput: "",
+        analyzeSyntax: NO_ANALYZE
+    },
+    {
+        
+    }
+];

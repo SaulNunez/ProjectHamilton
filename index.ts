374f708bfad94b30e345daddce0a3100356c8a67
Saul Nuñez <saul.nunez99@gmail.com>
2020-09-17 01:28:24 -0600

    Corregir un error que puede ser causado por los tipos de datos asignados que pasa una libreria

--- index.ts
+++ index.ts
@@ -27,7 +27,7 @@ const wss = new Server({ server });
 
 wss.on('connection', (ws: WebSocket) => {
     ws.on('message', async (message: MessageEvent) => {
-        const messageContents = JSON.parse(message.data.toString());
+        const messageContents = (typeof message.data === "string")? JSON.parse(message.data): '';
         console.log(`Message received: ${message}`);
 
         const { lobbyCode } = messageContents.payload;

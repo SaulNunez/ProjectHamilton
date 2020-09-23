a3092b24c052a9e7f5829d996cfeb2ce60e4809d
Saul Nuñez <saul.nunez99@gmail.com>
2020-09-23 11:23:34 -0600

    Agregar stubs de pruebas unitarias

---
+++ gameapi/puzzles/get_puzzle/test_puzzle_get.test.ts
@@ -0,0 +1,17 @@
+describe('probar obtener puzzle', () => {
+    test('probar con el mismo tipo', () => {
+
+    });
+    test('probar cambiar a siguiente tipo', () => {
+
+    });
+    test('probar sin un solo puzzle', () => {
+
+    });
+});
+
+describe('probar logica de dificultad', () => {
+    test("no subir nivel de dificulta si el puzzle anterior tomo mucho tiempo", () => {
+
+    });
+})
